2a2b644aa1fc5c3c62749a4f87811145b1e3196d
shahjalal <shah.jalal@reivo.jp.com>
2020-12-30 16:42:11 +0600

    script include global

--- public/vendor/js/demo.js
+++ public/vendor/js/demo.js
@@ -423,4 +423,37 @@
     $('.product-image-thumb.active').removeClass('active');
     $(this).addClass('active');
   });
+
+  $('.select_all').click(function(event) {   
+    if(this.checked) {
+      // Iterate each checkbox
+      $(':checkbox').each(function() {
+        this.checked = true;                        
+      });
+    } else {
+      $(':checkbox').each(function() {
+        this.checked = false;                       
+      });
+    }
+  });
+
+  $('.menu').click(function(event) {
+      var menuId = $(this).data('id');
+      if(this.checked) {
+        $('.parentMenu_'+menuId).each(function() {
+          this.checked = true; 
+        });
+        $('.childMenu_'+menuId).each(function() {
+          this.checked = true; 
+        });
+      }else{
+        $('.parentMenu_'+menuId).each(function() {
+          this.checked = false; 
+        });
+        $('.childMenu_'+menuId).each(function() {
+          this.checked = false; 
+        });
+      }
+    });
+
 })(jQuery)
